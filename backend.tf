df27160629ebd635913f684ee228686240f4bee1
Ganesh <160034348+ganesh3110@users.noreply.github.com>
2024-03-22 15:51:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,13 @@
-
+provider "aws" {
+    region = " "
+    shared_credentials_files = [ ]
+    profile = "configs"
+    
+}
+terraform "s3" {
+    bucket = "bucketstate"
+    dynamodb_table = "terraform_state_lock"
+    region = " "
+    profile = "configs"
+    shared_credentials_files ="[ ]"
+}
